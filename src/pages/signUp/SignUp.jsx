6ca2a54b71e4f97159cdfe91fcbff7a3edc1e651
lha0 <lha0@unist.ac.kr>
2024-06-28 17:32:28 +0900

    닉네임 marginTop 수정

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -12,7 +12,7 @@ export default function SignUp() {
           placeholder="NAME"
         />
       </div>
-      <div className="w-[350px] mt-[40px] flex justify-between items-center gap-[10px] text-[15px]">
+      <div className="w-[350px] mt-[20px] flex justify-between items-center gap-[10px] text-[15px]">
         <div>닉네임</div>
         <input
           className="w-[270px] h-[45px] px-4 py-2 rounded-2xl shadow-button"
